139937c6a1c89f1faca48b0ef0093fd114692625
Shamenox <Shamenox@gmail.com>
2018-08-17 19:39:37 +0200

    Neuer Titel

--- last_piece_left.js
+++ last_piece_left.js
@@ -11,3 +11,12 @@ function Appstart(){
 	
 	Helon.app = Hollowed.loop;
 }
+
+
+class Char{
+	constructor(name){
+		this.name = name;
+		this.pos = {x : -1, y : -1}
+		this.dir = 1; //1=north 2=east
+	}
+}
